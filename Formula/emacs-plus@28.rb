8472a6731b5bb01471888263e1fa8b3dd0071c23
Iain H <iain@spiralpoint.org>
2022-04-17 23:14:19 -0400

    Include patch to center lines vertically when using line spacing

--- Formula/emacs-plus@28.rb
+++ Formula/emacs-plus@28.rb
@@ -80,7 +80,8 @@ class EmacsPlusAT28 < EmacsBase
   local_patch "no-frame-refocus-cocoa", sha: "fb5777dc890aa07349f143ae65c2bcf43edad6febfd564b01a2235c5a15fcabd" if build.with? "no-frame-refocus"
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "d6ee159839b38b6af539d7b9bdff231263e451c1fd42eec0d125318c9db8cd92"
-
+  local_patch "option-to-center-lines-vertically-when-using-line-sp", sha: "e2b3794357e8b30eceebed7c27dac5dab6e1ffa226c04da7b4dae019550fb293"
+  
   #
   # Install
   #
